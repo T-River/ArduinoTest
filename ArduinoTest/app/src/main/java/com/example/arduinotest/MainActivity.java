f4e8986561c68cbbd831d67482295eed9e4d921f
numa <srasrasrarin7@gmail.com>
2019-10-07 10:22:21 +0900

    MainActivity.java

---
+++ ArduinoTest/app/src/main/java/com/example/arduinotest/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.arduinotest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TextViewの設定
+        TextView data =  findViewById(R.id.arduino);
+        TextView StartingMessage  = findViewById(R.id.Run);
+
+        data.setText(R.string.data);
+
+        //つながってるものから使える全部のデバイス見つける
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            return;
+        }
+
+        //Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            return;
+        }
+
+        UsbSerialPort port = driver.getPorts().get(0);
+        try {
+
+            port.open(connection);
+            port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+
+            byte buffer[] = new byte[1000];//受信したデータをいれるもの
+            int read = port.read(buffer,1000);
+            Log.d(TAG,"Read" + read);
+        } catch (IOException e) {
+
+        }finally{
+            try {
+                port.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
